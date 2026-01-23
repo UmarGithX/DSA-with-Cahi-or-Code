d6ad4f5b1638ded92f9a6ac6ef9aa07a6ef1fe2b
UmarGitX <muhammadumaruaya@gmail.com>
2026-01-23 18:42:53 +0500

    Enhance array examples and add type codes documentation

--- Array Lecture/array_lecture_01.py
+++ Array Lecture/array_lecture_01.py
@@ -1,15 +1,54 @@
-import array
+# import array #this is the basic way to import a module in python
+# import array as arr #this way we can name the array to arr with the help of "as" word
+from array import * #this way we dont need to write any name for using the dubule code.deep
 
-val = array.array('i',[1,2,3,4,5,6])
+val = array('i',[1,2,3,4,5,6,7,8,9,10])
 
 # print(val)
  
 #  using for loop to output the values
 print("1st for loop")
-for i in range(1,7):
+for i in range(1,len(val)):
     print(i, end=" ") #the last end=" " used for printing the number in one line like 1,2,3,4,5,6;
 
 # for loop 2nd method
-print("2nd for loop")
+print("\n2nd for loop")
 for i in val:
     print(i , end=" ")
+# in python we dont need to To specify how many memory we need to give its alutomativally give it.
+
+
+#----------------------------------------------------------------------------#
+
+# we can find the typecode of the existing array.
+print("\n the typecode is:",val.typecode)
+
+#to revurse the array 
+# val.reverse()
+# print(val)
+
+#reversing using for loop
+val.reverse()
+# print("Reversed array:")
+# for i in val:
+#     print(i)
+
+for i in range(0,len(val)):
+    print(val[i])
+
+#------------------------------------------------------------------------------#
+#inserting in array
+
+#the insert funcition will add the number in array .insert(2,13) the first 2 was the index nummber and the 13 was the element we want to inset in array;
+x = array('i',[11,12,14,15,16,17,19])
+x.insert(2,13)
+print(x)
+
+
+#to all the elememnt at the last
+x.append(20)
+print(x)
+# for i in x:
+#     print(i)
+
+to add a 
---
+++ python array type codes.md
@@ -0,0 +1,15 @@
+| Type Code | C Type             | Python Type | Description                      |
+| --------- | ------------------ | ----------- | -------------------------------- |
+| `'b'`     | signed char        | int         | Signed integer (1 byte)          |
+| `'B'`     | unsigned char      | int         | Unsigned integer (1 byte)        |
+| `'u'`     | Py_UNICODE         | str         | Unicode character *(deprecated)* |
+| `'h'`     | signed short       | int         | Signed short (2 bytes)           |
+| `'H'`     | unsigned short     | int         | Unsigned short (2 bytes)         |
+| `'i'`     | signed int         | int         | Signed integer                   |
+| `'I'`     | unsigned int       | int         | Unsigned integer                 |
+| `'l'`     | signed long        | int         | Signed long                      |
+| `'L'`     | unsigned long      | int         | Unsigned long                    |
+| `'q'`     | signed long long   | int         | Signed long long                 |
+| `'Q'`     | unsigned long long | int         | Unsigned long long               |
+| `'f'`     | float              | float       | Floating-point (4 bytes)         |
+| `'d'`     | double             | float       | Floating-point (8 bytes)         |
